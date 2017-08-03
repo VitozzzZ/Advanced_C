5509519d8f69df7e0f48120951bec9d3608e6b8a
VitozzzZ <zealucius@gmail.com>
2017-08-03 12:53:20 +0800

    Update 指针和数组.cpp

--- Courseware/指针和数组.cpp
+++ Courseware/指针和数组.cpp
@@ -23,3 +23,30 @@ int main()
         cout<<setw(3)<<*p--;
     return 0;
 }
+
+//用指针遍历二维数组
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[3][4]={1,3,5,7,9,11,13,15,17,19,21,23};
+    int *p;
+    for(p=&a[0][0];p<&a[0][0]+12;p++)
+    {
+        cout<<p<<' '<<*p<<endl;
+    }
+    return 0;
+}
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int a[3][4]={1,3,5,7,9,11,13,15,17,19,21,23};
+    int (*p)[4],i,j;
+    p=a;
+    cin>>i>>j;
+    cout<<setw(4)<<*(*(p+i)+j);
+    return 0;
+}
