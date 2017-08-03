4f1ea76ddbfbe59f98d90c3e029f1e1224614096
VitozzzZ <zealucius@gmail.com>
2017-08-03 15:09:08 +0800

    Update 指针和数组.cpp

--- Courseware/指针和数组.cpp
+++ Courseware/指针和数组.cpp
@@ -50,3 +50,43 @@ int main()
     cout<<setw(4)<<*(*(p+i)+j);//可用p[i][j]代替
     return 0;
 }
+
+
+//二维数组名引用
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int a[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
+    cout<<"a="<<a<<endl;
+    cout<<"&a[0]="<<&a[0]<<endl<<endl;
+    
+    cout<<"a+1="<<a+1<<endl;
+    cout<<"&a[0]+1="<<&a[0]+1<<endl<<endl;
+    
+    cout<<"*a="<<*a<<endl;
+    cout<<"a[0]="<<a[0]<<endl;
+    cout<<"&a[0][0]="<<&a[0][0]<<endl<<endl;
+    
+    cout<<"*a+1="<<*a+1<<endl;
+    cout<<"a[0]+1="<<a[0]+1<<endl;
+    cout<<"&a[0][0]+1="<<&a[0][0]+1<<endl<<endl;
+    
+    return 0;
+    
+}
+output:
+a=0x7fff5fbff6d0
+&a[0]=0x7fff5fbff6d0
+
+a+1=0x7fff5fbff6e0
+&a[0]+1=0x7fff5fbff6e0
+
+*a=0x7fff5fbff6d0
+a[0]=0x7fff5fbff6d0
+&a[0][0]=0x7fff5fbff6d0
+
+*a+1=0x7fff5fbff6d4
+a[0]+1=0x7fff5fbff6d4
+&a[0][0]+1=0x7fff5fbff6d4
